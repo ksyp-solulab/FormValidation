bc72bf5c9e6222b4abd672ee75a679514b3a3fc8
ksyp-solulab <shuklakashyap@solulab.com>
2022-08-26 11:44:34 +0530

    Added all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import rocketImg from "./assest/rocket.png";
+import SignUp from "./components/SignUp";
 
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-3">
+      <div className="row">
+        <div className="col-md-7">
+          {" "}
+          <SignUp />
+        </div>
+        <div className="col-md-5">
+          <img className="img-fluid" src={rocketImg} alt="Not found"></img>
+        </div>
+      </div>
     </div>
   );
 }
